2975a11e21b928afb4bcea421f5c6678f3586361
Aqsin21 <150579577+Aqsin21@users.noreply.github.com>
2025-12-02 23:44:55 +0400

    Extension Service Collection Setup

---
+++ LetsDo/LetsDo.BLL/Extension/BLLServiceCollection.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LetsDo.BLL.Extension
+{
+    public static class BLLServiceCollection
+    {
+     
+    }
+
+}
---
+++ LetsDo/LetsDo.DAL/Extension/DALServiceCollection.cs
@@ -0,0 +1,22 @@
+﻿using LetsDo.DAL.Repositories.Abstract;
+using LetsDo.DAL.Repositories.Concrete;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LetsDo.DAL.Extension
+{
+    public static class DalServiceCollection
+    {
+        public static IServiceCollection AddDal(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
+            return services;
+
+        }
+    }
+}
