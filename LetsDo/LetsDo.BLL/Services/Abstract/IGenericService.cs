beb951827ee6dc13e4485d24d96435b40bb86b0b
Aqsin21 <150579577+Aqsin21@users.noreply.github.com>
2025-12-02 23:40:46 +0400

    Generics Added

---
+++ LetsDo/LetsDo.BLL/Services/Abstract/IGenericService.cs
@@ -0,0 +1,14 @@
+﻿
+using System.Linq.Expressions;
+namespace LetsDo.BLL.Services.Abstract
+{
+    public interface IGenericService<T> where T : class
+    {
+        Task<IEnumerable<T>> GetAllAsync(string? includeProporties = null);
+        Task<T> GetByIdAsync(int id, string? includeProperties = null);
+        Task<bool> ExistsAsync(Expression<Func<T, bool>> predicate);
+        Task<T> CreateAsync(T entity);
+        Task<T> UpdateAsync(T entity);
+        Task<bool> DeleteAsync(int id);
+    }
+}
---
+++ LetsDo/LetsDo.BLL/Services/Concrete/GenericService.cs
@@ -0,0 +1,76 @@
+﻿using LetsDo.BLL.Services.Abstract;
+using LetsDo.DAL.DataContext;
+using LetsDo.DAL.Repositories.Abstract;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+namespace LetsDo.BLL.Services.Concrete
+{
+    public class GenericService<T> : IGenericService<T> where T : class
+    {
+        protected readonly IGenericRepository<T> _repository;
+        protected readonly AppDbContext _context;
+
+        public GenericService(IGenericRepository<T> repository, AppDbContext context)
+        {
+            _repository = repository;
+            _context = context;
+
+        }
+        public async Task<T> CreateAsync(T entity)
+        {
+            await _repository.AddAsync(entity);
+            await _repository.SaveChangesAsync();
+            return entity;
+        }
+
+        public async Task<bool> DeleteAsync(int id)
+        {
+            await _repository.DeleteAsync(id);
+            var changes = await _repository.SaveChangesAsync();
+            return changes > 0;
+        }
+
+        public async Task<bool> ExistsAsync(Expression<Func<T, bool>> predicate)
+        {
+            return await _context.Set<T>().AnyAsync(predicate);
+        }
+
+
+        public async Task<IEnumerable<T>> GetAllAsync(string? includeProporties = null)
+        {
+            IQueryable<T> query = _context.Set<T>();
+            if (!string.IsNullOrEmpty(includeProporties))
+            {
+                foreach (var includeProp in includeProporties.Split(',', StringSplitOptions.RemoveEmptyEntries))
+                {
+                    query = query.Include(includeProp.Trim());
+                }
+            }
+            return await query.ToListAsync();
+        }
+
+        public async Task<T?> GetByIdAsync(int id, string? includeProperties = null)
+        {
+            IQueryable<T> query = _context.Set<T>();
+
+            if (!string.IsNullOrEmpty(includeProperties))
+            {
+                foreach (var includeProp in includeProperties.Split(',', StringSplitOptions.RemoveEmptyEntries))
+                {
+                    query = query.Include(includeProp.Trim());
+                }
+            }
+
+            return await query.FirstOrDefaultAsync(e => EF.Property<int>(e, "Id") == id);
+        }
+
+        public async Task<T> UpdateAsync(T entity)
+        {
+            await _repository.UpdateAsync(entity);
+            await _repository.SaveChangesAsync();
+            return entity;
+
+        }
+    }
+
+}
